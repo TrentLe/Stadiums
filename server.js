1a9f319d2b259aa3959862f59b03f1231757c021
TrentLe <raeburn.trent6@gmail.com>
2023-02-13 20:58:39 -0500

    Fixed routes

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Setting handlebars.js as default engine
-app.engine('handlebars', exphbs.engine)
-app.set('view-engine', 'handlebars')
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(require('./controllers/index'))
+app.use(require('./controllers/index'))
 
 // turn on routes
 app.use(routes);
