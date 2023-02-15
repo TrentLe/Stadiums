5081ec7717ffa91ebbed3443cc80a7ee5e7dbbd2
TrentLe <raeburn.trent6@gmail.com>
2023-02-15 11:24:59 -0500

    routes added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const login = require('./login');
 const trips = require('./tripRoutes');
 const profile = require('./profile');
+const location = require('./locationRoutes');
 
 router.use('/profile', profile);
 router.use('/login', login);
 router.use('/trips', trips);
+router.use('/location', location);
 
 module.exports = router;
--- controllers/api/locationRoutes.js
+++ controllers/api/locationRoutes.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const { Location } = require('../models');
+const { Location } = require('../../models');
 
 routes.get('/', async (req, res) => {
     try {
@@ -10,15 +10,37 @@ routes.get('/', async (req, res) => {
         console.log(err)
     }
     });
+    
 
-routes.get('/:id', async (req, res) => {
-    try {
-        const locationData = await Location.findByPk(req.params.id);
-        res.status(200).json(locationData);
-    } catch (err) {
-        res.status(500).json(err);
-        console.log(err)
-    }
-    });
+    routes.get('/:sport', async (req, res) => {
+        try {
+            const locationData = await Location.findAll({
+                where: {
+                    sport: req.params.sport
+                }
+            });
+
+            const locations = locationData.map((location) => location.get({ plain: true }))
+
+            if (req.params.sport === 'MLB') {  
+                res.render('MLB', { locations });
+            } else if (req.params.sport === 'NFL') {
+                res.render('NFL', { locations });
+            } else if (req.params.sport === 'NBA') {
+                res.render('NBA', { locations });
+            } else if (req.params.sport === 'NHL') {
+                res.render('NHL', { locations });
+            } else  {
+                res.status(404).json({ message: 'No location with this sport!' });
+            }
+        } catch (err) {
+            res.status(500).json(err);
+            console.log(err)
+        }
+        });
+
+
+
+module.exports = routes;
 
     
